7f2a5576f375a69f00bb0b7ed4a1dd2bfb604fac
rsakibanda <ramesh4sr@gmail.com>
2017-10-23 22:28:35 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,22 @@
 function basicTeenager(age) {
-
+  return age>12 && age<20;
 }
 
 function teenager(age) {
-
+  return basicTeenager(age);
 }
 
 function ageChecker(age) {
-
+  return basicTeenager(age);
+  
 }
 
 function ternaryTeenager(age) {
-
+  return basicTeenager(age);
+  
 }
 
 function switchAge(age) {
-
+  return basicTeenager(age);
+  
 }
