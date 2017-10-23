237f3d0e40c427a50a421126884708cbf1fc3d66
rsakibanda <ramesh4sr@gmail.com>
2017-10-23 23:07:27 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -23,7 +23,16 @@ function ageChecker(age) {
 function ternaryTeenager(age) {
   return age>12 && age<20?'You are a teenager':'You are not a teenager';;
 }
-
+    
 function switchAge(age) {
-  return basicTeenager(age);
+  switch(age){
+    case 13:
+      case 14:
+        case 15:
+          case 16:
+            case 17:
+              case 18:
+                case 19:
+                return "You are a teenager"
+  }
 }
